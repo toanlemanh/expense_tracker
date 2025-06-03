a402fbd21d752d0362217ac23dd111eebdb38a0b
toanlemanh <chen22112k3@gmail.com>
2025-06-03 10:16:30 +0700

    feat: create transaction list holding records

---
+++ lib/presentation/expense_list/transaction_list.dart
@@ -0,0 +1,27 @@
+import 'package:expense_tracker/core/widgets/transaction_record.dart';
+import 'package:expense_tracker/main.dart';
+import 'package:flutter/material.dart';
+
+class TransactionList extends StatelessWidget {
+   late final ScrollController scrollController;
+  TransactionList({
+    super.key,
+    required this.scrollController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Placeholder for transaction items
+    return ListView.builder(
+      itemCount: items.length, // Replace with your transaction count
+      controller: scrollController,
+      itemBuilder: (context, index) {
+        // Replace with your transaction item widget
+        return TransactionRecord(
+          title: items[index],
+          itemColor: colors[index]
+        );
+      },
+    );
+  }
+}
