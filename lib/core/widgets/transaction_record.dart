f8504eb00dbad357fdcb83a12d37c940fe574d04
toanlemanh <chen22112k3@gmail.com>
2025-06-03 10:49:00 +0700

    feat: enable clickable transaction recordrecord

--- lib/core/widgets/transaction_record.dart
+++ lib/core/widgets/transaction_record.dart
@@ -39,65 +39,68 @@ class TransactionRecord extends StatelessWidget {
   }
 
   Widget buildTransactionTile(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 16.0, top: 8.0, bottom: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Color(itemColor), // sử dụng itemColor
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: ListTile(
-          // tileColor: Color(0xFFC7E9E8),
-          contentPadding: EdgeInsets.only(left: 8, right: 20),
-          dense: true, // giảm khoảng cách giữa các dòng
-            title: RichText(
-            text: TextSpan(
-              style: TextStyle(
-                color: appColors['astrograniteDebris'],
-                fontSize: 18,
-              ),
-              children: [
-              TextSpan(text: title),
-              WidgetSpan(
-                child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                child: Icon(Icons.food_bank, size: 18),
-                ),
-              ),
-              WidgetSpan(
-                child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                child: Icon(Icons.gamepad, size: 18),
-                ),
-              ),
-              ],
-            ),
-            ),
-          subtitle: const Text('23:05'),
-          leading: SizedBox(
-            width: 50,
-            height: 50,
-
-            child: Container(
-              decoration:
-              // foregroundDecoration:
-              BoxDecoration(
-                color: Color(0xff45e0fc), // lớp phủ đen mờ
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Center(child: Icon(Icons.shop, color: Colors.black)),
-            ),
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16.0, top: 8.0, bottom: 8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(itemColor), // sử dụng itemColor
+            borderRadius: BorderRadius.circular(20),
           ),
-          trailing: Text(
-            '-12000',
-            style: TextStyle(
-              color: appColors['fuzzyWuzzyBrown'],
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+          child: ListTile(
+            // tileColor: Color(0xFFC7E9E8),
+            contentPadding: EdgeInsets.only(left: 8, right: 20),
+            dense: true, // giảm khoảng cách giữa các dòng
+            title: RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: appColors['astrograniteDebris'],
+                  fontSize: 18,
+                ),
+                children: [
+                  TextSpan(text: title),
+                  WidgetSpan(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                      child: Icon(Icons.food_bank, size: 18),
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                      child: Icon(Icons.gamepad, size: 18),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            subtitle: const Text('23:05'),
+            leading: SizedBox(
+              width: 50,
+              height: 50,
+
+              child: Container(
+                decoration:
+                // foregroundDecoration:
+                BoxDecoration(
+                  color: Color(0xff45e0fc), // lớp phủ đen mờ
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(child: Icon(Icons.shop, color: Colors.black)),
+              ),
+            ),
+            trailing: Text(
+              '-12000',
+              style: TextStyle(
+                color: appColors['fuzzyWuzzyBrown'],
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
       ),
+      onTap: () => print(title),
     );
   }
 }
