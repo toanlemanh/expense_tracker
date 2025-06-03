24eca4423446fa89c66f96246056324a8b7c834d
toanlemanh <chen22112k3@gmail.com>
2025-06-03 16:05:44 +0700

    update: update route namesnames

--- lib/core/routing/router_configuration.dart
+++ lib/core/routing/router_configuration.dart
@@ -2,8 +2,12 @@ import 'package:expense_tracker/features/home/view/home_screen.dart';
 import 'package:expense_tracker/features/setting/view/setting_screen.dart';
 import 'package:expense_tracker/features/asset/view/wallet_screen.dart';
 import 'package:expense_tracker/presentation/expense_list/expense_list_page.dart';
+import 'package:expense_tracker/core/widgets/pop_up_page.dart';
+import 'package:expense_tracker/presentation/layout/category_page.dart';
+import 'package:expense_tracker/presentation/layout/ledger_page.dart';
 import 'package:expense_tracker/presentation/layout/single_scrollable_sheet.dart';
 import 'package:expense_tracker/presentation/statistic/money_statistic_page.dart';
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../main.dart';
@@ -24,15 +28,48 @@ final routerConfigurations = GoRouter(
     //     name: AppRoutes.home.name,
     //     builder: (context, state) => SingleScrollableSheet(child: ExpenseListPage()),
     //   ),
-    
+
     // ]),
     GoRoute(
-        path: AppRoutes.home.path,
-        name: AppRoutes.home.name,
-        builder: (context, state) {
-            return MainScaffold();
-          },
+      path: AppRoutes.home.path,
+      name: AppRoutes.home.name,
+      builder: (context, state) {
+        return MainScaffold();
+      },
+    ),
 
-      ),
+    GoRoute(
+      path: AppRoutes.ledgers.path,
+      name: AppRoutes.ledgers.name,
+      pageBuilder:
+          (context, state) => CustomTransitionPage(
+            child: LedgerPage(),
+            transitionsBuilder: _buildTransitionAnimation,
+          ),
+    ),
+    GoRoute(
+      path: AppRoutes.category.path,
+      name: AppRoutes.category.name,
+      pageBuilder:
+          (context, state) => CustomTransitionPage(
+            child: CategoryPage(),
+            transitionsBuilder: _buildTransitionAnimation,
+          ),
+    ),
   ],
-); 
+);
+
+Widget _buildTransitionAnimation(
+  context,
+  animation,
+  secondaryAnimation,
+  child,
+) {
+  const begin = Offset(0, 1); // từ dưới lên
+  const end = Offset.zero;
+  const curve = Curves.ease;
+
+  final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+  final offsetAnimation = animation.drive(tween);
+  return SlideTransition(position: offsetAnimation, child: child);
+}
