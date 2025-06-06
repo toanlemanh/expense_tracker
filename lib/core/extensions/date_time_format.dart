de9ef2dcc60639bc202b4297c951d20e4ff4663f
toanlemanh <chen22112k3@gmail.com>
2025-06-06 09:11:40 +0700

    fix: create a list of weekday

--- lib/core/extensions/date_time_format.dart
+++ lib/core/extensions/date_time_format.dart
@@ -1,4 +1,4 @@
-extension DateTimeFormat on DateTime{
+extension DateTimeFormat on DateTime {
   String getHourLabel() {
     //returns hour in 'HH:00' format
     return '${hour.toString().padLeft(2, '0')}:00';
@@ -7,18 +7,26 @@ extension DateTimeFormat on DateTime{
   String getDateLabel() {
     //returns hour in 'May 28' format
     const months = [
-    '', // index 0 unused
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ];
+      '', // index 0 unused
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December',
+    ];
+    print('Month: $month Day: $day');
     return '${months[month]} $day';
   }
 
-    String getWeekDayLabel() {
+  String getWeekDayLabel() {
     //returns hour in 'Wed' format
     const List<String> weekdays = [
-      'mon', 'tue', 'wed', 'thu', 'fri', 'sad', 'sun'
+      'mon',
+      'tue',
+      'wed',
+      'thu',
+      'fri',
+      'sad',
+      'sun',
     ];
-    return weekdays[weekday-1];
+    return weekdays[weekday - 1];
   }
+
 }
