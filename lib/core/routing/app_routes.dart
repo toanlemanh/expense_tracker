9fb546408e5e66be1ceeab0201c6f9d1c9d8e926
toanlemanh <chen22112k3@gmail.com>
2025-06-07 22:39:54 +0700

    feat: create bookmarks route

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -1,5 +1,5 @@
 enum AppRoutes {
-  home('/home'), ledgers('/ledgers'), category('/category')
+  home('/home'), ledgers('/ledgers'), category('/category'), bookmarks('/bookmarks')
   // trang details cho tat ca cac page detail va truyen vao child
   //tất cả nằm ở trong /home
   // còn lại sẽ là các route đi từ  home ra 
