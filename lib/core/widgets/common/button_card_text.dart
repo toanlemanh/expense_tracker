86ad25b71bff75943d483fa0e238398f972f03bc
toanlemanh <chen22112k3@gmail.com>
2025-06-07 22:42:41 +0700

    update: text to richtext

--- lib/core/widgets/common/button_card_text.dart
+++ lib/core/widgets/common/button_card_text.dart
@@ -3,7 +3,7 @@ import 'package:expense_tracker/data/sharedpref/shared_preference_color.dart';
 import 'package:flutter/material.dart';
 
 class ButtonCardText extends StatelessWidget {
-  final Text buttonText;
+  final RichText buttonText;
   final String imagePath;
   final VoidCallback? onTap;
   final Size? size;
@@ -20,35 +20,38 @@ class ButtonCardText extends StatelessWidget {
   Widget build(BuildContext context) {
     final backgroundColor = ImageColorStore().getAveriColorLightest(imagePath);
 
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: size?.width,
-        height: size?.height,
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(24),
-        ),
-        child: Row(
-          mainAxisSize: size == null ? MainAxisSize.min : MainAxisSize.max,
-          children: [
-            // Icon/Image
-            CardImageSquare(
-              imagePath: imagePath,
-              autoBackground: true,
-              onTap: onTap,
-            ),
-            const SizedBox(width: 8),
-            // Text
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                buttonText
-              ],
-            ),
-          ],
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: size?.width,
+          height: size?.height,
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: Row(
+            mainAxisSize: size == null ? MainAxisSize.min : MainAxisSize.max,
+            children: [
+              // Icon/Image
+              CardImageSquare(
+                imagePath: imagePath,
+                autoBackground: true,
+                onTap: onTap,
+              ),
+              const SizedBox(width: 8),
+              // Text
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // buttonText
+                  Expanded(child: buttonText),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
