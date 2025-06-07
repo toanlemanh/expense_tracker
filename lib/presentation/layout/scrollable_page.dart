614965964f7667adca9b72f4bc2227d2761e54d0
toanlemanh <chen22112k3@gmail.com>
2025-06-07 22:43:30 +0700

    update: change to button card text under

--- lib/presentation/layout/scrollable_page.dart
+++ lib/presentation/layout/scrollable_page.dart
@@ -1,7 +1,6 @@
 import 'package:expense_tracker/core/colors/app_colors.dart';
 import 'package:expense_tracker/core/routing/app_routes.dart';
-import 'package:expense_tracker/core/widgets/transaction_record.dart';
-import 'package:expense_tracker/main.dart';
+import 'package:expense_tracker/core/widgets/common/button_card_text_under.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -85,64 +84,41 @@ class _ScrollablePageState extends State<ScrollablePage>
           // lam tam cac nut bam co the chuyen sang trang khac (dung go router)
           // configure sau nay co the them vao mot muc moi de dang (Toan)
           // Lam 1 trang detail sample, moi ledger co mot type => truyen url 1 type
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 14),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () => context.go(AppRoutes.ledgers.path),
-                      child: const Center(child: Text('Ledger')),
-                    ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ButtonCardTextUnder(
+                    imagePath: 'assets/icons/categories/png/Journal.png',
+                    onTap: () => context.go(AppRoutes.ledgers.path),
+                    buttonText: const Text('Ledger'),
+                    size: Size(30,30),
+                    
                   ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 14),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () => context.go(AppRoutes.category.path),
-                      child: const Center(child: Text('Category')),
-                    ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: ButtonCardTextUnder(
+                    imagePath: 'assets/icons/categories/png/Vector-22.png',
+                    onTap: () => context.go(AppRoutes.category.path),
+                    buttonText: const Text('Category'),
+                     size: Size(30,30),
                   ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 14),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () => print('clickme Bookmarks'),
-                      child: const Center(child: Text('Bookmarks')),
-                    ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child:  ButtonCardTextUnder(
+                    imagePath: 'assets/icons/categories/png/Vector-13.png',
+                    onTap: () => context.go(AppRoutes.bookmarks.path),
+                    buttonText: const Text('Bookmarks'),
+                    size: Size(30,30),
                   ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 14),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () => print('clickme Bookmarks'),
-                      child: const Center(child: Text('Bookmarks')),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+                // const SizedBox(width: 20),
+               ],
             ),
+          ),
 
           // Transaction list trong này
           // Transaction nhận 2 biến pagekey (theo idex) và scrollController của DraggableScrollableSheet
