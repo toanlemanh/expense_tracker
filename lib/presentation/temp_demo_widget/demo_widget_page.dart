2eb31612a0ca529dbe31bacb2c6264f03ca68f36
toanlemanh <chen22112k3@gmail.com>
2025-06-07 22:43:45 +0700

    update: change text to richtext

--- lib/presentation/temp_demo_widget/demo_widget_page.dart
+++ lib/presentation/temp_demo_widget/demo_widget_page.dart
@@ -22,10 +22,6 @@ class DemoWidgetPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           const Text('Đề mô quít chét (s)'),
-          ButtonCardText( buttonText: Text('Button Card Text'),
-                          imagePath: 'assets/icons/categories/png/Car.png',
-                          onTap: _testOnTap
-                        ),
           CardImageSquare(
             imagePath: 'assets/icons/categories/png/Car.png',
             autoBackground: true,
