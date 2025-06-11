b96d2148d55ffaabef61abc921459a46d4efd80d
toanlemanh <chen22112k3@gmail.com>
2025-06-11 16:57:34 +0700

    update: use future builder to build transaction record

--- lib/presentation/expense_list/transaction_list.dart
+++ lib/presentation/expense_list/transaction_list.dart
@@ -1,5 +1,7 @@
 import 'package:expense_tracker/core/widgets/transaction_record.dart';
 import 'package:expense_tracker/core/widgets/transaction_summary.dart';
+import 'package:expense_tracker/features/record/model/record_type.dart';
+import 'package:expense_tracker/features/record/service/record_type_service.dart';
 import 'package:expense_tracker/features/record/viewmodel/record_store.dart';
 import 'package:expense_tracker/main.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +15,8 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     late final recordStore = Provider.of<RecordStore>(context);
+    late final recordTypeService = Provider.of<RecordTypeService>(context);
+
     // Placeholder for transaction items
     //TODO: create a collector gradually count income, expense and reset
     //each time meeting a transaction summary and stop.
@@ -21,51 +25,71 @@ class TransactionList extends StatelessWidget {
           (context) => ListView.builder(
             itemCount: recordStore.length,
             controller: scrollController,
-            itemBuilder: (context, index) {
-              List<Widget> widgets = [];
-
-              final isIncome = recordStore.records[index].recordTypeId >= 29;
-              // Always add the TransactionRecord
-              widgets.add(
-                TransactionRecord(
-                  title: '${recordStore.records[index].money}',
-                  itemColor: colors[index],
-                  //${recordStore.records[index].recordTypeId}
-                  imagePath: 'assets/icons/categories/png/Car.png',
-                  itemCreateHour: recordStore.records[index].createTime,
-                  willBuildIndicator:
-                      recordStore.filteredRecords[index].isFirstHourItem,
-                  money: recordStore.records[index].money,
-                  isIncome: isIncome,
-                ),
-              );
-
-              // Additionally add TransactionSummary if condition is true
-              if (recordStore.filteredRecords[index].isFirstDateItem) {
-                int _id = index;
-                int _summaryIncome = 0;
-                int _summaryExpense = 0;
-                do {
-                  recordStore.records[_id].recordTypeId >= 29
-                  ? _summaryIncome += recordStore.records[_id].money
-                  : _summaryExpense += recordStore.records[_id].money;
-                  _id++;
-                } while (_id < recordStore.length && !recordStore.filteredRecords[_id].isFirstDateItem);
-                widgets.insert(
-                  0,
-                  TransactionSummary(
-                    income: _summaryIncome,
-                    expense: _summaryExpense,
-                    createTime: recordStore.records[index].createTime,
-                  ),
-                );            
-              }
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: widgets,
-              );
-            },
+            itemBuilder:
+                (context, index) =>
+                    _buildRecord(recordStore, recordTypeService, index),
           ),
+    );
+  }
+
+
+//build future widget because get record type is future 
+  Widget _buildRecord(
+    RecordStore recordStore,
+    RecordTypeService recordTypeService,
+    int index,
+  ) {
+    return FutureBuilder<RecordType>(
+      future: recordTypeService.getRecordType(
+        recordStore.records[index].recordTypeId,
+      ),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SizedBox.shrink();
+        }
+        final recordType = snapshot.data!;
+        final isIncome = recordStore.records[index].recordTypeId >= 29;
+        List<Widget> widgets = [];
+
+        widgets.add(
+          TransactionRecord(
+            title: '${recordType.name?.substring(10)}',
+            itemColor: colors[index], // Use your color logic
+            imagePath: 'assets/icons/categories/png/Car.png',
+            itemCreateHour: recordStore.records[index].createTime,
+            willBuildIndicator:
+                recordStore.filteredRecords[index].isFirstHourItem,
+            money: recordStore.records[index].money,
+            isIncome: isIncome,
+          ),
+        );
+
+        if (recordStore.filteredRecords[index].isFirstDateItem) {
+          int _id = index;
+          int _summaryIncome = 0;
+          int _summaryExpense = 0;
+          do {
+            recordStore.records[_id].recordTypeId >= 29
+                ? _summaryIncome += recordStore.records[_id].money
+                : _summaryExpense += recordStore.records[_id].money;
+            _id++;
+          } while (_id < recordStore.length &&
+              !recordStore.filteredRecords[_id].isFirstDateItem);
+          widgets.insert(
+            0,
+            TransactionSummary(
+              income: _summaryIncome,
+              expense: _summaryExpense,
+              createTime: recordStore.records[index].createTime,
+            ),
+          );
+        }
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: widgets,
+        );
+      },
     );
   }
 }
